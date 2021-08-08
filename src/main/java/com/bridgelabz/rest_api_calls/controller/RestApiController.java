a07642dad073e22514d5c273af4afa1b74c79112
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-08 19:40:52 +0530

    Created: Rest call to show hello message by passing query parameter

--- src/main/java/com/bridgelabz/rest_api_calls/controller/RestApiController.java
+++ src/main/java/com/bridgelabz/rest_api_calls/controller/RestApiController.java
@@ -1,10 +1,10 @@
 /*
- * UC-1:- Create Rest controller to demonstrate the various HTTP methods
- * and Respond hello messages to the user.
- * To Begin with show hello message form BridgeLabz.
- *  -Use GET Request method.
- *  -Use Curl to demonstrate REST API Calls.
- *  -curl localhost:8080/hello -w "/n".
+ * UC-2 :- Make REST call to show Hello Mark form BridgeLabz.
+ *  -Use GET Request method and pass and pass name as query parameter.
+ *  -Use Curl to Demonstrate REST API call.
+ *  -curl localhost:8080/hello/query?name=Mark -w "/n"
+ *  eg.  curl -X GET "http://localhost:8080/hello/query/?name=Chandradip" -w "/"
+
  *
  * @Author : Chandradip Shivankar
  * @Since : August 08, 2021
@@ -12,11 +12,13 @@
 
 package com.bridgelabz.rest_api_calls.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class RestApiController {
 
     /**
@@ -25,8 +27,20 @@ public class RestApiController {
      * @RequestMapping (method = RequestMethod.GET).
      * @return hello form the BridgeLabz.
      */
-    @GetMapping("/hello")
+
+    @RequestMapping(value = {"", "/", "/home"} )
     public String hello() {
-        return "Hello form BridgeLabz";
+        return "Hello form BridgeLabz.";
+    }
+
+    /**
+     * Annotation for mapping HTTP GET requests onto specific handler methods.
+     * @param fName;
+     * @param lName;
+     * @return Hello fName lName.
+     */
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "fName") String fName, @RequestParam(value = "lName") String lName) {
+        return "Hello " + fName + " " + lName + ".";
     }
 }
